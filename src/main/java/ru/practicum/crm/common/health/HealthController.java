99afb7eaa337bc13b0a8cf1a6b284d5266e025fb
Nikolay Aleksandrov <nkl.aleks@yandex.ru>
2026-09-11 14:27:43 +0300

    style(T-011): форматирование кода по Checkstyle

--- src/main/java/ru/practicum/crm/common/health/HealthController.java
+++ src/main/java/ru/practicum/crm/common/health/HealthController.java
@@ -1,9 +1,8 @@
 package ru.practicum.crm.common.health;
 
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class HealthController {
