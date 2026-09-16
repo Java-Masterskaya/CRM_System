21c2a96da5965af0e4b6378cece75f0c2135d794
Efirios <ASmir006@yandex.ru>
2026-09-16 10:57:57 +0300

    feat(T-010): генерация OpenAPI и страница документации

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
 
 tasks.withType<Test> {
